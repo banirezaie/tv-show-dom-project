0e6524f8115ebf6c8f7e1a61c4da95fe95764475
BANI GIT <bani.rezaie@gmail.com>
2020-04-22 10:38:15 +0100

    just started

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 //You can edit ALL of the code here
+
 function setup() {
   const allEpisodes = getAllEpisodes();
+  namesOfEpisodes(allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+}
+
+function namesOfEpisodes(param) {
+  let div = document.getElementById("root");
+  for (let i = 0; i < param.length; i++) {
+    let nameOfMovie = param[i].name;
+    console.log(div);
+    let paragraph = document.createElement(`p`);
+    paragraph.textContent = "names" + nameOfMovie;
+    div.appendChild(paragraph);
+  }
 }
 
 window.onload = setup;
