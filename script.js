59acbde540b43205b16c793645e03ce4c33d1318
BANI GIT <bani.rezaie@gmail.com>
2020-04-23 20:17:13 +0100

    level 100

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
 //You can edit ALL of the code here
-
 function setup() {
   const allEpisodes = getAllEpisodes();
-  namesOfEpisodes(allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  episodeList.forEach((episode) => {
+    let eachDiv = document.createElement(`div`);
+    let containerDiv = rootElem.appendChild(eachDiv);
+    containerDiv.setAttribute(`id`, `container`);
+    containerDiv.setAttribute(`class`, `container`);
 
-function namesOfEpisodes(param) {
-  let div = document.getElementById("root");
-  for (let i = 0; i < param.length; i++) {
-    let nameOfMovie = param[i].name;
-    console.log(div);
-    let paragraph = document.createElement(`p`);
-    paragraph.textContent = "names" + nameOfMovie;
-    div.appendChild(paragraph);
+    //take the name of each episode and put it inside containerDiv
+    let nameParagraph = document.createElement(`p`);
+    containerDiv.appendChild(nameParagraph);
+    nameParagraph.setAttribute(`id`, `name`);
+    nameParagraph.setAttribute(`class`, `name`);
+    nameParagraph.textContent = `${episode.name}`;
+
+    //take the episode code
+    let episodeCodeP = document.createElement(`p`);
+    containerDiv.appendChild(episodeCodeP);
+    episodeCodeP.setAttribute(`id`, `episodeCode`);
+    episodeCodeP.setAttribute(`class`, `episodeCode`);
+    episodeCodeP.textContent = `Episode Code: S${codeCorrection(
+      episode.season
+    )}E${codeCorrection(episode.number)}`;
+
+    //take the episode Images
+    let episodeImg = document.createElement(`img`);
+    let divImg = document.createElement(`div`);
+    divImg.appendChild(episodeImg);
+    containerDiv.appendChild(divImg);
+    divImg.setAttribute(`class`, `divImg`);
+    episodeImg.setAttribute(`id`, `episodeImg`);
+    episodeImg.setAttribute(`class`, `episodeImg`);
+    episodeImg.src = episode.image.medium;
+    episodeImg.alt = `image`;
+
+    //take the summary of each episode
+    let episodeSummary = document.createElement(`article`);
+    let summaryArticle = containerDiv.appendChild(episodeSummary);
+    episodeSummary.setAttribute(`id`, `summary`);
+    episodeSummary.setAttribute(`class`, `summary`);
+
+    //summary tag inside article
+    let summaryTagP = document.createElement(`p`);
+    summaryArticle.appendChild(summaryTagP);
+    summaryTagP.setAttribute(`id`, `summaryTag`);
+    summaryTagP.textContent = `SUMMARY:`;
+
+    //actual summary inside article
+    let summary = document.createElement(`p`);
+    summaryArticle.appendChild(summary);
+    summary.setAttribute(`id`, `summary`);
+    summary.setAttribute(`class`, `summary`);
+    summary.textContent = episode.summary.replace(/(<([^>]+)>)/gi, "");
+  });
+}
+function codeCorrection(x) {
+  if (x < 10) {
+    return `0${x}`;
+  } else {
+    return x;
   }
 }
-
 window.onload = setup;
