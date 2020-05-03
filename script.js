84c2e66c1e119c46d2ac6f2b23b64ea2c60fec00
BANI GIT <bani.rezaie@gmail.com>
2020-05-03 02:30:02 +0100

    end of the level 400

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 //You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-  search();
-  result();
-  home();
-  select(allEpisodes);
+
+function getData(URL) {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((json) => {
+      makeShowPage(json);
+      selectShow(json);
+      setup(`https://api.tvmaze.com/shows/${json[0].id}/episodes`);
+      search();
+      result();
+      home();
+    });
 }
 
-function makePageForEpisodes(episodeList) {
+function setup(URL) {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((json) => {
+      makeEpisodePage(json);
+      selectEpisode(json);
+    });
+}
+
+function makeEpisodePage(list) {
   const rootElem = document.getElementById("root");
-  episodeList.forEach((episode) => {
+  list.forEach((episode) => {
     let eachDiv = document.createElement(`div`);
     let containerDiv = rootElem.appendChild(eachDiv);
     containerDiv.setAttribute(`id`, `${episode.id}`);
@@ -92,6 +106,58 @@ function makePageForEpisodes(episodeList) {
     summary.textContent = episode.summary.replace(/(<([^>]+)>)/gi, "");
   });
 }
+
+function makeShowPage(list) {
+  const rootElem = document.getElementById("root");
+  list.forEach((show) => {
+    let eachDiv = document.createElement(`div`);
+    let containerDiv = rootElem.appendChild(eachDiv);
+    containerDiv.setAttribute(`id`, `${show.id}`);
+    containerDiv.setAttribute(`class`, `container`);
+
+    //take the name of each show and put it inside containerDiv
+    let nameParagraph = document.createElement(`p`);
+    containerDiv.appendChild(nameParagraph);
+    nameParagraph.setAttribute(`class`, `name`);
+    nameParagraph.textContent = `${show.name}`;
+    let br = document.createElement(`br`);
+    let br2 = document.createElement(`br`);
+    containerDiv.appendChild(br2);
+
+    //take the show Images
+    let showImg = document.createElement(`img`);
+    let divImg = document.createElement(`div`);
+    divImg.appendChild(showImg);
+    containerDiv.appendChild(divImg);
+    divImg.setAttribute(`class`, `divImg`);
+    showImg.setAttribute(`class`, `showImg`);
+    showImg.src = show.image.medium;
+    showImg.alt = `image`;
+    containerDiv.appendChild(br);
+
+    //take the summary of each show
+    let showSummary = document.createElement(`article`);
+    let summaryArticle = containerDiv.appendChild(showSummary);
+    showSummary.setAttribute(`class`, `summary`);
+
+    //summary tag inside article
+    let summaryTagP = document.createElement(`p`);
+    summaryArticle.appendChild(summaryTagP);
+    summaryTagP.setAttribute(`class`, `summaryTag`);
+    summaryTagP.textContent = `About ${show.name}`;
+
+    //actual summary inside article
+    let summary = document.createElement(`p`);
+    summaryArticle.appendChild(summary);
+    summary.setAttribute(
+      `id`,
+      `summaryPS${codeCorrection(show.season)}E${codeCorrection(show.number)}`
+    );
+    summary.setAttribute(`class`, `summaryP`);
+    summary.textContent = show.summary.replace(/(<([^>]+)>)/gi, "");
+  });
+}
+
 function codeCorrection(x) {
   if (x < 10) {
     return `0${x}`;
@@ -99,6 +165,7 @@ function codeCorrection(x) {
     return x;
   }
 }
+
 function search() {
   let form = document.createElement(`form`);
   form.setAttribute(`id`, `searchForm`);
@@ -160,14 +227,14 @@ function result() {
 }
 
 // select option part
-function select(episode) {
+function selectEpisode(episode) {
   let select = document.createElement(`select`);
   header.appendChild(select);
   select.setAttribute(`id`, `episodeSelect`);
   select.setAttribute(`class`, `select`);
   let firstOption = document.createElement(`option`);
   select.appendChild(firstOption);
-  firstOption.setAttribute(`id`, `firstOption`);
+  firstOption.setAttribute(`id`, `firstEpisode`);
   firstOption.setAttribute(`value`, `select an option`);
   firstOption.textContent = `Episode Selector:`;
   episode.forEach((x) => {
@@ -185,11 +252,54 @@ function select(episode) {
     let containerDiv = document.getElementsByClassName("container");
     container = Array.from(containerDiv);
     let resultCounter = document.getElementById(`result`);
-    if (select.value === e.target.value) {
-      container.map((x) => (x.style.display = `none`));
-      targetDiv.style.display = `initial`;
-      resultCounter.textContent = `01 / ${codeCorrection(container.length)}`;
+    container.map((x) => (x.style.display = `none`));
+    targetDiv.style.display = `initial`;
+    resultCounter.textContent = `01 / ${codeCorrection(container.length)}`;
+  });
+}
+
+function selectShow(show) {
+  let select = document.createElement(`select`);
+  header.appendChild(select);
+  select.setAttribute(`id`, `showSelect`);
+  select.setAttribute(`class`, `select`);
+  let firstOption = document.createElement(`option`);
+  select.appendChild(firstOption);
+  firstOption.setAttribute(`id`, `firstShow`);
+  firstOption.setAttribute(`value`, `select an option`);
+  firstOption.textContent = `Show Selector:`;
+
+  //sorting  in alphabetical order case-insensitive
+  let showArray = Array.from(show);
+  let sort = showArray.sort(function (a, b) {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) {
+      return -1;
     }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  sort.forEach((x) => {
+    let option = document.createElement(`option`);
+    option.setAttribute(`value`, x.id);
+    option.setAttribute(`class`, `option`);
+    select.appendChild(option);
+    option.textContent = `${x.name} - Genre: ${x.type}`;
+  });
+
+  //changes after select a show on show selector
+  select.addEventListener(`change`, (e) => {
+    let targetDiv = document.getElementById(`${e.target.value}`);
+    let containerDiv = document.getElementsByClassName("container");
+    container = Array.from(containerDiv);
+    // let resultCounter = document.getElementById(`result`);
+    container.map((x) => (x.style.display = `none`));
+    setup(`https://api.tvmaze.com/shows/${select.value}/episodes`);
+    // resultCounter.textContent = `01 / ${codeCorrection(container.length)}`;
   });
 }
 
@@ -202,4 +312,8 @@ function home() {
   anchor.textContent = `..Home`;
 }
 
-window.onload = setup;
+function onLoad() {
+  getData(`https://api.tvmaze.com/shows`);
+}
+
+window.onload = onLoad;
